cc377478f5f75486f1561e6bc31d4b2aaf26fa92
Joshua Mugisha <joshuamugisha@MacBook-Pro-90.local>
2019-09-06 09:57:52 +0300

    add styling to the productsoverview screen

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FlatList, Text } from 'react-native';
 import { useSelector } from 'react-redux';
+import ProductItem from '../../components/shop/ProductItem';
 
 const ProductsOverviewScreen = props => {
   const products = useSelector(state => state.products.availableProducts);
@@ -8,7 +9,13 @@ const ProductsOverviewScreen = props => {
     <FlatList
       data={products}
       keyExtractor={item => item.id}
-      renderItem={itemData => <Text>{itemData.item.title}</Text>}
+      renderItem={itemData => (
+        <ProductItem
+          title={itemData.item.title}
+          image={itemData.item.imageUrl}
+          price={itemData.item.price}
+        />
+      )}
     />
   );
 };
