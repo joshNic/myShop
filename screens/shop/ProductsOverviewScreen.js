a3fb403f7938cdf8acb876a5d8110b6686815985
Joshua Mugisha <joshuamugisha@MacBook-Pro-90.local>
2019-09-06 13:02:44 +0300

    implement adding to cart

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import { FlatList, Text } from 'react-native';
-import { useSelector } from 'react-redux';
+import { FlatList } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
+
 import ProductItem from '../../components/shop/ProductItem';
+import * as cartActions from '../../store/actions/cart';
 
 const ProductsOverviewScreen = props => {
   const products = useSelector(state => state.products.availableProducts);
+  const dispatch = useDispatch();
+
   return (
     <FlatList
       data={products}
       keyExtractor={item => item.id}
       renderItem={itemData => (
         <ProductItem
-          title={itemData.item.title}
           image={itemData.item.imageUrl}
+          title={itemData.item.title}
           price={itemData.item.price}
           onViewDetail={() => {
             props.navigation.navigate('ProductDetail', {
@@ -20,7 +24,9 @@ const ProductsOverviewScreen = props => {
               productTitle: itemData.item.title
             });
           }}
-          onAddToCart={() => {}}
+          onAddToCart={() => {
+            dispatch(cartActions.addToCart(itemData.item));
+          }}
         />
       )}
     />
