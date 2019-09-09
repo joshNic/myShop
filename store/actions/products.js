6e88d7f733b5426c2fbfd55d7f321562f2105b4a
Joshua Mugisha <joshuamugisha@MacBook-Pro-90.local>
2019-09-09 14:14:31 +0300

    add token to signed in users

--- store/actions/products.js
+++ store/actions/products.js
@@ -42,19 +42,28 @@ export const fetchProducts = () => {
 };
 
 export const deleteProduct = productId => {
-  return async dispatch => {
-    await fetch(`https://partcraze.firebaseio.com/products/${productId}.json`, {
-      method: 'DELETE'
-    });
+  return async (dispatch, getState) => {
+    const token = getState().auth.token;
+    const response = await fetch(
+      `https://partcraze.firebaseio.com/products/${productId}.json?auth=${token}`,
+      {
+        method: 'DELETE'
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error('Something went wrong!');
+    }
     dispatch({ type: DELETE_PRODUCT, pid: productId });
   };
 };
 
 export const createProduct = (title, description, imageUrl, price) => {
-  return async dispatch => {
+  return async (dispatch, getState) => {
     // any async code you want!
+    const token = getState().auth.token;
     const response = await fetch(
-      'https://partcraze.firebaseio.com/products.json',
+      `https://partcraze.firebaseio.com/products.json?auth=${token}`,
       {
         method: 'POST',
         headers: {
@@ -85,18 +94,26 @@ export const createProduct = (title, description, imageUrl, price) => {
 };
 
 export const updateProduct = (id, title, description, imageUrl) => {
-  return async dispatch => {
-    await fetch(`https://partcraze.firebaseio.com/products/${id}.json`, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        title,
-        description,
-        imageUrl
-      })
-    });
+  return async (dispatch, getState) => {
+    const token = getState().auth.token;
+    const response = await fetch(
+      `https://partcraze.firebaseio.com/products/${id}.json?auth=${token}`,
+      {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          title,
+          description,
+          imageUrl
+        })
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error('Something went wrong!');
+    }
 
     dispatch({
       type: UPDATE_PRODUCT,
